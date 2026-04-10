a6852f957fe2f85b5a3138161dcfc31c7cb715be
ʟᴇᴋʀᴇᴛʜꜱ <163004654+Lekreths@users.noreply.github.com>
2026-04-10 09:45:05 +0500

    Implement power function and salary summation
    Fix syntax errors and improve salary summation logic.

---
+++ 10.04.26.js
@@ -0,0 +1,66 @@
+// Рекурсия - это функция, которая в процессе выполнения может вызываться себя повторно. 
+
+// 1 Cпособ - интеративынй: 
+let x = 2;
+let n = 3;
+
+function pow(x,n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+alert( pow(2,3) );
+
+// 2 Способ - рекурсивный:
+
+function pow(x,n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x,n - 1)
+  }
+}
+
+alert( pow(2,3) );
+
+// 2 задание с объектами и массивами 
+let company = {
+  sales: [{
+    name: "John",
+    salary: 1000
+  }, {
+    name: "Ivan",
+    salary: 700
+  }],
+
+  development: {
+    sities: [{
+      name; "Alice",
+      salary: 800
+    }, {
+      name: "Daniel",
+      salary: 900,
+    }],
+    internals: [{
+      name: "Jack",
+      salary: 800
+    }]
+  }
+};
+
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    return depatment.reduce((prev, current) => prev + current.salary, 0);
+  } else {
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep);
+    }
+    return sum;
+  }
+}
