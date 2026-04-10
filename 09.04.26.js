d8e7406e87d71fd39de1f26272f9bbfa48608587
ʟᴇᴋʀᴇᴛʜꜱ <163004654+Lekreths@users.noreply.github.com>
2026-04-10 09:53:18 +0500

    Add array method examples for forEach and indexOf
    Added examples of forEach and indexOf methods in arrays.

--- 09.04.26.js
+++ 09.04.26.js
@@ -13,3 +13,17 @@ alert(arr3) //2,3
 // delete: can delete the element in massive, but when you need use length, then can be same length, then before
 // splice: can delete the element on an index, an element or 's
 // splice: can copy elements with use index and element, like 1 index start - and 3 index to end 
+
+let arr4 = [1,2,3,4]
+arr4.forEach(alert)
+arr4.forEach(function(item, index, array) = {
+  alert(`The ${item} has index ${index} in ${array}`);
+})
+
+let arr5 = [1,2,3,4]
+alert(arr.indexOf(2)); // 1
+alert(arr.indexOf(4)); // 3
+alert(arr.indexOf(null)) // -1 
+
+// Be careful, in there used by ===, then if he found "false", this is correctly "false", not zero 0
+
