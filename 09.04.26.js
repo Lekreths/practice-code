c11465715a052b3f390ac27b6b246e83cb9a24dd
ʟᴇᴋʀᴇᴛʜꜱ <163004654+Lekreths@users.noreply.github.com>
2026-04-10 10:10:50 +0500

    Fix syntax errors and improve comments in 09.04.26.js
    Corrected syntax errors in forEach and alert calls, and updated comments for clarity.

--- 09.04.26.js
+++ 09.04.26.js
@@ -16,14 +16,49 @@ alert(arr3) //2,3
 
 let arr4 = [1,2,3,4]
 arr4.forEach(alert)
-arr4.forEach(function(item, index, array) = {
+arr4.forEach(function(item, index, array) {
   alert(`The ${item} has index ${index} in ${array}`);
 })
 
 let arr5 = [1,2,3,4]
-alert(arr.indexOf(2)); // 1
-alert(arr.indexOf(4)); // 3
-alert(arr.indexOf(null)) // -1 
+alert(arr5.indexOf(2)); // 1
+alert(arr5.indexOf(4)); // 3
+alert(arr5.indexOf(null)) // -1 
 
 // Be careful, in there used by ===, then if he found "false", this is correctly "false", not zero 0
+
+let arr6 = [1,2,3,4]
+alert(arr6.includes(1) // True
+alert(arr6.includes(3) // True 
+alert(arr6.includes(5) // False 
+
+// forEach - is method, where every element in array used "separately"
+// indexOF - is method, where from massive find the correctly element.
+// includes - it is like indexOf, but in return write: True, False
+
+let arr7 = [
+  {id: 1, number: 1},
+  {id: 2, number: 2},
+  {id: 3, number: 3},
+  {id: 4, number: 1}
+;
+
+// 1.1 find
+let result = arr7.find(item => item.id == 1);
+alert(result.number);
+// 1.2 find 
+let result = arr7.find(function(item) {
+  return item.id == 1;
+})
+alert(result)
+
+// 2.1 findIndex
+alert(arr7.findIndex(number => arr7.name == 2)) // 1
+// 2.2 findLastIndex
+alert(arr7.findLastIndex(number => arr7.name == 1)) // 3
+
+// find - is a method, where from array with objects find corrctly massive
+
+
+
 
