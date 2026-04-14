79771c866445428feeb72ad04fb5625fdac62ae4
ʟᴇᴋʀᴇᴛʜꜱ <163004654+Lekreths@users.noreply.github.com>
2026-04-14 23:46:35 +0500

    Add bubble sort function to 10.04.26.js

--- 10.04.26.js
+++ 10.04.26.js
@@ -64,3 +64,19 @@ function sumSalaries(department) {
     return sum;
   }
 }
+
+
+
+
+
+function bubbleSort(arr) {
+  for (let j = arr.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if(arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
+    }
+  }
+}
